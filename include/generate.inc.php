5b7438e873f5414fa088c96b748d2d41ed324fc7
rileyjamesbell <bellriley5@gmail.com>
2020-01-17 09:56:56 -0800

    Use norefferer on email

--- include/generate.inc.php
+++ include/generate.inc.php
@@ -175,7 +175,7 @@ function buildPost($post,$res=0){
                         $tip="onmouseover=\"Tip('<img src=\\'data:image/jpg;base64,".$badge."\\' alt=\\'\\'/>');\" onmouseout=\"UnTip();\" ";
                 $postinfo.="<a charset=\"UTF-8\" href=\"".$post["steam"]."\" target=\"_blank\"><img ".$tip."src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAA7DAAAOwwHHb6hkAAABHElEQVQ4jd2SMYrCUBCGvxcWooVC2ngBLQRPoJWIYJc+hxDEwkoEi7Se4HVibyPiGTyBjU16IRCI799icdmwFq5a7cBUM98/8w9jAAcYngsZQE/CAHivwG8R+Hi0sVarEccxxhistVwuF+DreA/dYLFY4HkekjDGMJvNyhuMRiM6nQ7n8xlrbQmuVCpUq1XG4zEASZL8ttDtdhkOh4RhSBAErNdr0jQFYDAY0O/3WS6XSMI5VxogQKvVSsfjUbcoikKHw0GbzUan00nOOV2vV83nc/m+rxv3LVCv1xVFkZrNpuI41na7VZ7nkqQsy7Tf7zWdTn+CZYF72Wg0ZK1VkiTa7XZqtVp/EwDUbrc1mUzU6/Xu1v/BK3u8aOETWfygttVpCfcAAAAASUVORK5CYII=\" alt=\"Steam\"/></a> ";
         }
-        if($post["email"])$post["name"]="<a href=\"email:".$post["email"]."\">".$post["name"]."</a>";
+        if($post["email"])$post["name"]="<a rel=\"noreferrer\" href=\"email:".$post["email"]."\">".$post["name"]."</a>";
         if($post["trip"]||$post["name"])$postinfo.="<span class=\"nameBlock\">";
 	if($post["name"])$postinfo.="<b class=\"name\">".$post["name"]."</b>";
         if($post["trip"])$postinfo.="<span class=\"postertrip\">".$post["trip"]."</span>";
